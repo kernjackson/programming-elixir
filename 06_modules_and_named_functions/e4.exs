0c817d28e5739c00112d778b7e74ad1eac4e8314
kernjackson <kernjackson@gmail.com>
2015-12-07 19:18:36 -0600

    Added exercise 4 and most of exercise 5

---
+++ 06_modules_and_named_functions/e4.exs
@@ -0,0 +1,17 @@
+defmodule Exercises do
+  # Exercise 4: calculate the sum of the integers from 1 to n
+  def sum(0), do: 0
+  def sum(n), do: n + sum(n-1)
+
+  # Exercise 5: Finds the greatest common divisor
+  ## is negative? No idea how to do this one
+  ## are both numbers zero?
+  ## is y zero? gcd(x,y) -> x
+  ## gcd(y, rem(x,y))
+
+  #  def gcd(-x,-y), do: 0
+  def gcd(0,0), do: 0
+  def gcd(x,0), do: x
+  def gcd(x,y), do: gcd(y, rem(x,y))
+  
+end

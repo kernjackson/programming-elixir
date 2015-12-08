12f6337306d3b1de03a91082b9b2df731f8bb6ed
kernjackson <kernjackson@gmail.com>
2015-12-07 19:31:18 -0600

    Added check for negative numbers on x and y

--- 06_modules_and_named_functions/e4.exs
+++ 06_modules_and_named_functions/e4.exs
@@ -10,6 +10,8 @@ defmodule Exercises do
   ## gcd(y, rem(x,y))
 
   #  def gcd(-x,-y), do: 0
+  def gcd(x,_) when (x < 0), do: IO.puts "negative number"
+  def gcd(_,y) when (y < 0), do: IO.puts "negative number"
   def gcd(0,0), do: 0
   def gcd(x,0), do: x
   def gcd(x,y), do: gcd(y, rem(x,y))

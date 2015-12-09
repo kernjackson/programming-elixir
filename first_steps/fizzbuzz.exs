a5bd6f9a6cfbc2824dc2986732e9bf28a98709d0
kernjackson <kernjackson@gmail.com>
2015-12-08 23:14:08 -0600

    Fixed bug in fizzbuzz solution

--- first_steps/fizzbuzz.exs
+++ first_steps/fizzbuzz.exs
@@ -4,7 +4,7 @@ defmodule FizzBuzz do
 
   def fizz(0, 0, _) do: IO.puts "FizzBuzz"
   def fizz(0, _, _) do: IO.puts "Fizz"
-  def fizz(0, 0, _) do: IO.puts "Buzz"
+  def fizz(_, 0, _) do: IO.puts "Buzz"
   def fizz(_, _, c) do: IO.puts c
 
   def buzz(n) do fizz( rem(n,3), rem(n,5), n ) end
